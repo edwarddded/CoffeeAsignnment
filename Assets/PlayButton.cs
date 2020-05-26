b5cf3dafef9cb3dae297269eb831f3591035e84f
edward yang <53891309+edwarddded@users.noreply.github.com>
2020-05-26 13:21:14 +1000

    Tutoral Level Edward 26/05/2020

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void BackLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitLevel()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
